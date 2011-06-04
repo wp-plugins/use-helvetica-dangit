af8c319328376b83fa98c473761aef90d13ed9cf
mattwiebe <mattwiebe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-04 16:50:35 +0000

    Saving the world from Arial. Or, at least WordPress.
    git-svn-id: https://plugins.svn.wordpress.org/use-helvetica-dangit/trunk@393134 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== Plugin Name ===
+Contributors: mattwiebe
+Tags: admin typography helvetica
+Requires at least: 3.2beta
+Tested up to: 3.2beta
+Stable tag: 1.0
+
+Nobody should have to look at Arial.
+
+== Description ==
+
+WordPress 3.2 uses Arial in the admin, marring an otherwise beautiful design. Use Helvetica instead instead of its bastard offspring.
+
+== Frequently Asked Questions ==
+
+= Will this plugin remove Arial from the WordPress admin area? =
+
+Yes.
+
+= Can you also remove the scourge of Arial from the rest of the Internet? =
+
+If only I could. Be the change you want to see in the world.
+
+= I don't see the plugin listed =
+
+If you're on a Multisite Network, activate this plugin across the network in the Network admin area so that all of your users can bask in the glow of improved typography.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release.
---
+++ use-helvetica-dangit.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Plugin Name: Use Helvetica Dangit
+Plugin URI: http://somadesign.ca/
+Description: WordPress 3.2 uses Arial in the admin. It might steal sheep too.
+Author: Soma Design
+Author URI: http://somadesign.ca/
+Version: 1.0
+Network: true
+*/
+/*
+Copyright (c) Soma Design. http://somadesign.ca/
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+add_action( 'admin_head', 'use_helvetica_dangit' );
+function use_helvetica_dangit() { ?>
+<style type="text/css"> body, td, textarea, input, select {
+	font-family: 'Helvetica Neue', 'Helvetica Neue LT Std', 'Helvetica LT Std', Helvetica, sans-serif; } </style>
+<?php
+}	
